f5f05249a54f4c61344b46abfa0fb89526a8b262
Victor Astakhov <work.lisophoria@gmail.com>
2023-05-15 00:08:29 +0300

    add project dao & controller, add no args constructor to Project class

--- src/main/java/com/lisophoria/projectTrackerBackend/model/Project.java
+++ src/main/java/com/lisophoria/projectTrackerBackend/model/Project.java
@@ -2,11 +2,13 @@ package com.lisophoria.projectTrackerBackend.model;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
+@NoArgsConstructor
 public class Project {
     private Long projectId;
     private String projectName;
